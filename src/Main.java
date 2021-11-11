f259c930f7335b5ecf157a9762957429229f123e
JeremyDAdams <Beoron@gmail.com>
2021-11-11 15:58:33 +0000

    Build basic log in GUI.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LogIn.fxml"));
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
 }
