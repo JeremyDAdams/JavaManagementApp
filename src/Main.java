a64a4a26fdec04d44e81d0adc6e11382a8d11aa5
JeremyDAdams <Beoron@gmail.com>
2021-11-12 02:28:34 +0000

    Add language switching to login page.

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
 
+    static Stage stage;
+
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/LogIn.fxml"));
-        stage.setScene(new Scene(root, 300, 275));
+        this.stage = stage;
+        Locale.setDefault(new Locale("fr", "fr-fr"));
+        ResourceBundle rb = ResourceBundle.getBundle("language_files/rb");
+
+        Parent main = null;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogIn.fxml"));
+        loader.setResources(rb);
+        main = loader.load();
+
+        Scene scene = new Scene(main);
+
+        stage.setScene(scene);
+
         stage.show();
+        /*Parent root = FXMLLoader.load(getClass().getResource("/view/LogIn.fxml"));
+        loader.setResource(rb);
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();*/
     }
     public static void main(String[] args) {
     System.out.println(Locale.getDefault());
         launch(args);
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 }
--- src/controller/LogInController.java
+++ src/controller/LogInController.java
@@ -1,4 +1,42 @@
 package controller;
 
-public class LogInController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LogInController implements Initializable {
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Label userLabel;
+
+    @FXML
+    private Label headingLabel;
+
+    @FXML
+    private Label locationLabel;
+
+    ResourceBundle rb;
+
+    //@Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.rb = rb;
+        System.out.println(Locale.getDefault());
+
+        passwordLabel.setText(rb.getString("password"));
+        submitButton.setText(rb.getString("submit"));
+        userLabel.setText(rb.getString("user"));
+        headingLabel.setText(rb.getString("heading"));
+    }
+
 }
