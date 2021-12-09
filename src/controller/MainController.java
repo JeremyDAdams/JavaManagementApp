6fdb83e0d085fb1cd0890166cf972bf41446af4e
JeremyDAdams <Beoron@gmail.com>
2021-12-09 02:21:24 +0000

    Fill out Main.fxml and configure exit button.

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import utilities.JDBC;
+
 public class MainController {
+    public void exitBtnClick(ActionEvent actionEvent) {
+        JDBC.closeConnection();
+        System.exit(0);
+    }
 }
