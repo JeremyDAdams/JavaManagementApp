ddbc49f264a8dd6650aaa42f772fd7f947493393
JeremyDAdams <Beoron@gmail.com>
2021-12-29 08:43:58 +0000

    Create and begin work on AddCustomer.fxml

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,13 +2,19 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.Customer;
 import utilities.JDBC;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,6 +22,11 @@ import static utilities.sqlCustomer.getAllCustomers;
 import static utilities.sqlCustomer.getCustomers;
 
 public class MainController implements Initializable {
+
+    Stage stage;
+    Parent scene;
+
+    public static Customer customerSelected;
 
     @FXML
     private TableView<Customer> customerTableView;
@@ -34,6 +45,29 @@ public class MainController implements Initializable {
         int userId = LogInController.userId;
         System.out.println(userId + "This is from MainController");
 
+    }
+
+
+    public void custAddBtnClick(ActionEvent actionEvent) throws IOException {
+        stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/AddCustomer.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    public void custUpdateBtnClick(ActionEvent actionEvent) {
+    }
+
+    public void custDeleteBtnClick(ActionEvent actionEvent) {
+    }
+
+    public void apptAddBtnClick(ActionEvent actionEvent) {
+    }
+
+    public void apptUpdateBtnClick(ActionEvent actionEvent) {
+    }
+
+    public void apptDeleteBtnClick(ActionEvent actionEvent) {
     }
 
     public void exitBtnClick(ActionEvent actionEvent) {
