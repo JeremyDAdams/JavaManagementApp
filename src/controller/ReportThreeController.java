cf7f799d66fce66e7d27fb563fa23dde7e6d3aa5
JeremyDAdams <Beoron@gmail.com>
2022-01-24 21:11:13 +0000

    Create ReportThree.fxml and ReportThreeController. Begin adding functionality.

---
+++ src/controller/ReportThreeController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ReportThreeController {
+    Parent scene;
+
+    @FXML
+    public Label createdByAdmin;
+
+    @FXML
+    public Label createdByTest;
+
+    @FXML
+    public Label createdByScript;
+
+    public void backBtnClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+}
