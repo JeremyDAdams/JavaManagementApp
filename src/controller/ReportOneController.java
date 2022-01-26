9cd52a57c2c98dc91a9caf0523b1c6ca59e2d257
JeremyDAdams <Beoron@gmail.com>
2022-01-26 21:10:54 +0000

    Make and integrate gui for report one.

--- src/controller/ReportOneController.java
+++ src/controller/ReportOneController.java
@@ -1,8 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import utilities.JDBC;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,6 +21,7 @@ import java.util.ResourceBundle;
 
 public class ReportOneController implements Initializable {
 
+    Parent scene;
     int numberInJan;
     int numberInFeb;
     int numberInMar;
@@ -32,6 +42,61 @@ public class ReportOneController implements Initializable {
     int numberTest;
     int numberOther;
 
+    @FXML
+    public Label janLbl;
+
+    @FXML
+    public Label febLbl;
+
+    @FXML
+    public Label marLbl;
+
+    @FXML
+    public Label aprLbl;
+
+    @FXML
+    public Label mayLbl;
+
+    @FXML
+    public Label junLbl;
+
+    @FXML
+    public Label julLbl;
+
+    @FXML
+    public Label augLbl;
+
+    @FXML
+    public Label sepLbl;
+
+    @FXML
+    public Label octLbl;
+
+    @FXML
+    public Label novLbl;
+
+    @FXML
+    public Label decLbl;
+
+    @FXML
+    public Label planningSessionLbl;
+
+    @FXML
+    public Label debriefingLbl;
+
+    @FXML
+    public Label strategyLbl;
+
+    @FXML
+    public Label planningLbl;
+
+    @FXML
+    public Label testLbl;
+
+    @FXML
+    public Label otherLbl;
+
+
     private static Connection connection = JDBC.getConnection();
     static Statement statement = null;
     static String appointmentQueryMonth = "SELECT COUNT(Appointment_ID), DATE_FORMAT(start, '%M') FROM appointments GROUP BY MONTH(start)";
@@ -44,11 +109,31 @@ public class ReportOneController implements Initializable {
             throwables.printStackTrace();
         }
 
+        janLbl.setText(janLbl.getText() + " " + numberInJan);
+        febLbl.setText(febLbl.getText() + " " + numberInFeb);
+        marLbl.setText(marLbl.getText() + " " + numberInMar);
+        aprLbl.setText(aprLbl.getText() + " " + numberInApr);
+        mayLbl.setText(mayLbl.getText() + " " + numberInMay);
+        junLbl.setText(junLbl.getText() + " " + numberInJun);
+        julLbl.setText(julLbl.getText() + " " + numberInJul);
+        augLbl.setText(augLbl.getText() + " " + numberInAug);
+        sepLbl.setText(sepLbl.getText() + " " + numberInSep);
+        octLbl.setText(octLbl.getText() + " " + numberInOct);
+        novLbl.setText(novLbl.getText() + " " + numberInNov);
+        decLbl.setText(decLbl.getText() + " " + numberInDec);
+
         try {
             typeCount();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+
+        planningSessionLbl.setText(planningSessionLbl.getText() + " " + numberPlanningSession);
+        debriefingLbl.setText(debriefingLbl.getText() + " " + numberDebriefing);
+        strategyLbl.setText(strategyLbl.getText() + " " + numberStrategy);
+        planningLbl.setText(planningLbl.getText() + " " + numberPlanning);
+        testLbl.setText(testLbl.getText() + " " + numberTest);
+        otherLbl.setText(otherLbl.getText() + " " + numberOther);
     }
 
     public void monthCount() throws SQLException {
@@ -106,5 +191,12 @@ public class ReportOneController implements Initializable {
                 numberOther = resultSet.getInt(1);
             }
         }
+    }
+
+    public void backBtnClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 }
